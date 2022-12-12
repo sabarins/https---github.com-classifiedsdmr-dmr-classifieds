092eb37989190536508e80a1577d4f802a139b1d
SABARI S <ssabari1492000@gmail.com>
2022-12-12 16:45:52 +0530

    updated designs

--- loginpages/Components/APILogin/APILog.js
+++ loginpages/Components/APILogin/APILog.js
@@ -3,19 +3,21 @@ import axios from "axios";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 import cookie from "js-cookie";
-import Content from "./BodyContent/Content";
+import Content from "../../pages/Login";
+import { useRouter } from 'next/router'
 
 
 export default function indexLogin({ token }) {
+  let router = useRouter();
   // const { data: session } = useSession()
 
   // console.log({session});
 
-//   useEffect(() => {
-//     console.log(sessionStorage);
-//     sessionStorage.setItem("name",name);
-//     console.log(sessionStorage.getItem("name"));
-//   });
+  //   useEffect(() => {
+  //     console.log(sessionStorage);
+  //     sessionStorage.setItem("name",name);
+  //     console.log(sessionStorage.getItem("name"));
+  //   });
 
   const showlogin = () => {
     document.getElementById("hideshow").style.display = "block";
@@ -26,7 +28,15 @@ export default function indexLogin({ token }) {
 
   let [status, setStatus] = useState("");
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  })
+
   function send() {
+
     axios
       .get(`https://rss.dinamalar.com/internal/send_otp.php?mobile=${number}`, {
         method: "POST",
@@ -35,12 +45,20 @@ export default function indexLogin({ token }) {
       .then((res) => {
         console.log("added", res);
       });
-    document.getElementById("submsg").style.display = "block";
+    document.getElementById("phandsubbtn").style.display = "none";
+    // document.getElementById("submsg").style.display = "block";
     document.getElementById("otpcontent").style.display = "block";
+
+    document.getElementById("senttext").style.display = "block";
+    document.getElementById('edit').style.display = "block";
+
+    document.getElementById("deschide").style.display = "none";
+
   }
 
   function otpverification() {
     let otp = document.getElementById("getotp").value;
+    document.getElementById('deschide').style.display = "none";
     console.log(number, otp);
     fetch(
       `https://rss.dinamalar.com/internal/otpverify.php?mobile=${number}&lg_otp=${otp}`
@@ -51,23 +69,41 @@ export default function indexLogin({ token }) {
         console.log(resp);
       });
 
-    
-     
-  }
-    if(status==="1")
-    {
-        localStorage.setItem("name",name);
-    console.log(localStorage.getItem("name"));
-        document.getElementById("loginon").style.display = "none";
-        document.getElementById("logout").style.display = "block";
+    localStorage.setItem("name", name);
 
-    }
+
+
+  }
+  if (status === "0") {
+    // document.getElementById("submsg").style.display = "none";
+
+    document.getElementById('numberinp').value = "";
+    document.getElementById('getotp').value = "";
+  }
+  if (status === "1") {
+    // document.getElementById("submsg").style.display = "none";
+    document.getElementById("otpcontent").style.display = "none";
+    document.getElementById('hidetxtnumandeditbtn').style.display = "none";
+    document.getElementById('nameinp').style.display = "block";
+
+    // router.push('/Login');
+  }
+
+
+  // if(status==="1")
+  // {
+  //     localStorage.setItem("name",name);
+  // console.log(localStorage.getItem("name"));
+  //     document.getElementById("loginon").style.display = "none";
+  //     document.getElementById("logout").style.display = "block";
+
+  // }
 
   console.log(status);
 
-  
 
- let [lstorage,setLstorage] = useState();
+
+  let [lstorage, setLstorage] = useState();
 
 
   return (
@@ -84,8 +120,8 @@ export default function indexLogin({ token }) {
           >
             Login
           </button>
-          <Content />
-          <button
+          {/* <div>{status === "1" && <Content data={localStorage.getItem("name")} />}</div> */}
+          {/* <button
             onClick={(e) => {
               fetch("/api/logout", {
                 method: "post",
@@ -104,7 +140,7 @@ export default function indexLogin({ token }) {
             style={{ marginLeft: "20px", display:"none", backgroundColor: "red" }}
           >
             Logout
-          </button>
+          </button> */}
         </div>
 
         <div className="rui-3edbr Fy4_1" id="hideshow">
@@ -117,6 +153,7 @@ export default function indexLogin({ token }) {
                 data-aut-id="icon"
                 className
                 fillRule="evenodd"
+                onClick={() => { document.getElementById("hideshow").style.display = "none"; }}
               >
                 <path
                   className
@@ -126,102 +163,108 @@ export default function indexLogin({ token }) {
             </span>
             <div className="_2nMat">
               <span data-aut-id="enteruser-click-back" className="_1nc4-">
-                {/* <svg width="20px" height="20px" viewBox="0 0 1024 1024" data-aut-id="icon" className fillRule="evenodd">
-                            <path className="rui-32D-k" d="M512 124.16v54.827l-302.293 294.187 676.949 0.043 38.827 38.784-38.827 38.784h-676.907l302.251 294.229v54.869h-56.32l-370.347-360.448v-54.869l370.347-360.405h56.32z" />
-                        </svg> */}
+
               </span>
               <div className="_2326- m-5 text-center">
-                <h1> Dinamalar Classifieds </h1>
+                <h1 style={{ marginTop: "50px", textAlign: "center" }}> Dinamalar Classifieds </h1>
               </div>
-              {/* <h3 className="_2SLog">
-              <span>Enter your phone number</span>
-            </h3> */}
+
               <div style={{ marginTop: "60px" }}>
                 <div className="rui-3APY9">
-                  <input
-                    onChange={(e) => {
-                      setName(e.target.value);
-                    }}
-                    name="phone"
-                    type="text"
-                    placeholder="Enter Your Name"
-                    className="rui-3vs1L rui-2LyaK undefined"
-                    id="nameinp"
-                  />
+
                   <div className="rui-1pgaV rui-Vcp5d" />
                 </div>
               </div>
-              <div className="_2PVVq">
-                <div className="rui-3OXDo">
-                  <label htmlFor="phone" />
-                  <div className="rui-2zp0U rui-2rouh">
-                    <div className="rui-z4oOZ rui-3i1AN">
-                      <div className="rui-3zt7h rui-iU02L rui-WrCgP">+91</div>
-                      <div className="rui-3APY9">
-                        <input
-                          onChange={(e) => {
-                            setNumber(e.target.value);
-                          }}
-                          name="phone"
-                          type="text"
-                          autoComplete="mobile"
-                          placeholder="Phone Number"
-                          className="rui-3vs1L rui-2LyaK undefined"
-                        />
-                        <div className="rui-1pgaV rui-Vcp5d" />
+              <div id="phandsubbtn">
+                <div className="_2PVVq" >
+                  <div className="rui-3OXDo" >
+                    <label htmlFor="phone" />
+                    <div className="rui-2zp0U rui-2rouh">
+                      <div className="rui-z4oOZ rui-3i1AN">
+                        <div className="rui-3zt7h rui-iU02L rui-WrCgP">+91</div>
+                        <div className="rui-3APY9">
+                          <input
+                            onChange={(e) => {
+                              setNumber(e.target.value);
+                            }}
+                            name="phone"
+                            type="text"
+                            autoComplete="mobile"
+                            placeholder="Phone Number"
+                            className="rui-3vs1L rui-2LyaK undefined"
+                            id="numberinp"
+                          />
+                          <div className="rui-1pgaV rui-Vcp5d" />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <p>
-                    <button className="verifybtn" onClick={send}>
-                      SUBMIT
-                    </button>
-                  </p>
-                  <p id="submsg" style={{ display: "none", color: "green" }}>
-                    OTP has been sent from entered phone number!
-                  </p>
-                  <div id="otpcontent" className="otpbody">
-                    <div>
-                      <input
-                        type="text"
-                        className="otpinp"
-                        placeholder="Enter OTP"
-                        id="getotp"
-                      ></input>
-                      {/* <button onClick={otpverification}>Submit</button> */}
-                    </div>
-                    <button
-                      type="submit"
-                      onClick={() => {
-                        otpverification();
-                        if (status === "1") {
-                          console.log(status);
-                          document.getElementById("hideshow").style.display =
-                            "none";
-                        }
-                        fetch("/api/login", {
-                          method: "post",
-                          headers: {
-                            "Content-Type": "application/json",
-                          },
-                          body: JSON.stringify({ token: number }),
-                        });
-                      }}
-                      className="rui-39-wj rui-3mpqt rui-1JPTg _2sWUW "
-                    >
-                      <span style={{ color: "white" }}>VERIFY</span>
-                    </button>
-                  </div>
-                  <div className="rui-1rV1O">
-                    <span
-                      className="rui-3FLBC rui-_74YY"
-                      data-aut-id="error-phone"
-                    />
+                    <p>
+                      <button className="verifybtn" onClick={send}>
+                        SUBMIT
+                      </button>
+                    </p>
                   </div>
                 </div>
               </div>
 
-              <p className="_3bCAe">
+              {/* <p id="submsg" style={{ display: "none", color: "green" }}>
+                  OTP has been sent from entered phone number!
+                </p> */}
+              <div id="hidetxtnumandeditbtn" style={{ display: "flex", columnGap: "20px", fontSize: "15px" }}>
+                <div>
+                  <p id="senttext" style={{ display: "none" }}>We sent a 4 digit code to <b>+91{number}</b></p>
+                </div>
+                <div>
+                  <button id="edit" style={{ display: "none" }} className="editnum">Edit</button>
+                </div>
+              </div>
+              <div id="otpcontent" className="otpbody">
+                {status === "0" && (
+                  <p
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      color: "red",
+                      fontWeight: "bolder"
+                    }}
+                  >
+                    Invalid OTP, Try Again!
+                  </p>
+                )}
+                <div>
+                  <input
+                    type="text"
+                    className="otpinp"
+                    placeholder="Enter OTP"
+                    id="getotp"
+                  ></input>
+                  {/* <button onClick={otpverification}>Submit</button> */}
+                </div>
+                <button style={{ marginTop: "20px" }}
+                  type="submit"
+                  onClick={() => {
+                    otpverification();
+                    // if (status === "1") {
+                    //   console.log(status);
+                    //   document.getElementById("submsg").style.display =
+                    //     "none";
+                    // }
+
+                  }}
+                  className="rui-39-wj rui-3mpqt rui-1JPTg _2sWUW "
+                >
+                  <span>VERIFY</span>
+                </button>
+              </div>
+              <div className="rui-1rV1O">
+                <span
+                  className="rui-3FLBC rui-_74YY"
+                  data-aut-id="error-phone"
+                />
+              </div>
+
+
+              <p id="deschide" className="_3bCAe">
                 <span>
                   Your contact number is never shared with external parties nor
                   do we use it to spam you in any way.
@@ -235,26 +278,51 @@ export default function indexLogin({ token }) {
                     color: "green",
                   }}
                 >
-                  Successfully Verified!
+                  <b>OTP Verified Successfully !</b>
                 </p>
               )}
 
-              {status === "0" && (
-                <p
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    color: "red",
+              <div id="nameinp" style={{display:"none"}}>
+                <input
+                  onChange={(e) => {
+                    setName(e.target.value);
                   }}
-                >
-                  Invalid OTP, Try Again!
-                </p>
-              )}
+                  name="phone"
+                  type="text"
+                  placeholder="Enter Your Name"
+                  className="rui-3vs1L rui-2LyaK undefined"
+                  id="nameinp"
+                />
+                <p id="alertname" style={{display:"none",color:"red",fontWeight:"bolder",textAlign:"center"}}>Please Enter Your Name !</p>
+                <div>
+                <button id="continue" className="namebtn" style={{marginTop:"200px"}} onClick={()=>{
+                  if(name==="" || name.trim==="")
+                  {
+                    document.getElementById("alertname").style.display = "block";
+                  }
+                  else{
+                    router.push('./Login')}
+                  }}>
+                 Continue
+                </button>
+                </div>
+              </div>
+
+              {/* {status === "0" && (
+                  <p
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      color: "red",
+                    }}
+                  >
+                    Invalid OTP, Try Again!
+                  </p>
+                )} */}
             </div>
           </div>
         </div>
       </div>
-      <div>{status === "1" && <Content data={localStorage.getItem("name")} />}</div>
     </div>
   );
 }
--- loginpages/pages/Login.js
+++ loginpages/pages/Login.js
@@ -1,98 +1,50 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 
-export default function Logins() {
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import APILogin from '../Components/APILogin/APILog'
 
-    let [postid, setPostid] = useState('')
 
-    let [number, setNumber] = useState("");
+export default function Content(props) {
 
-    function send() {
-        axios.get(`https://rss.dinamalar.com/internal/send_otp.php?mobile=${number}`,
-            {
-                method: 'POST',
-                data: JSON.stringify({ mobile: number })
-            }).then((res) => {
-                console.log("added", res);
-            })
-    }
+let [gname,setGname] = useState("");
 
-    function otpverification() {
-        let otp = document.getElementById("getotp").value;
-        console.log(number, otp);
-        fetch(`https://rss.dinamalar.com/internal/otpverify.php?mobile=${number}&lg_otp=${otp}`)
-            .then(response => response.json())
-            .then((resp) => {
-                console.log(resp);
-            })
+let router = useRouter();
 
-    }
+useEffect(()=>
+{
+  if(props.data != "")
+{
+  var a = localStorage.getItem("name");
+  setGname(a);
+  document.getElementById("successdis").innerHTML = "Successfully LogIn!.."
+  setTimeout(()=>
+  {
+   document.getElementById("successdis").innerHTML = ""
 
-    // useEffect(() => {
-    //     // POST request using fetch inside useEffect React hook
-    //     const requestOptions = {
-    //         method: 'POST',
-    //         headers: { 'Content-Type': 'application/json' },
-    //         body: JSON.stringify({ title: 'React Hooks POST Request Example' })
-    //     };
-    //     fetch('https://jsonplaceholder.typicode.com/posts', requestOptions)
-    //         .then(response => response.json())
-    //         .then(data => setPostid(data.title));
-    // })
-    return (
-        <div>
-            <div style={{ display: "flex", justifyContent: "center" }}>
-                {/* <p>{number}</p> */}
-                <p><input type="number" onChange={(e) => { setNumber(e.target.value) }}></input></p>
-                <p><button onClick={send}>Verify</button></p>
-                <div className='otpbody'>
-                    <div>
-                        <input type="text" id='getotp'></input>
-                        <button onClick={otpverification}>Submit</button>
-                    </div>
-                </div>
-            </div>
-            <div className="rui-3edbr Fy4_1" id='hideshow'>
-                <div className="ZspKa"><span className="-_djW" data-aut-id="btnClose"><svg width="25px" height="25px" viewBox="0 0 1024 1024" data-aut-id="icon" className fillRule="evenodd"><path className d="M878.336 85.333l-366.336 366.315-366.336-366.315h-60.331v60.331l366.336 366.336-366.336 366.336v60.331h60.331l366.336-366.336 366.336 366.336h60.331v-60.331l-366.315-366.336 366.315-366.336v-60.331z" /></svg></span>
-                <form>
-                    <div className="_2nMat"><span data-aut-id="enteruser-click-back" className="_1nc4-">
-                        <svg width="20px" height="20px" viewBox="0 0 1024 1024" data-aut-id="icon" className fillRule="evenodd">
-                            <path className="rui-32D-k" d="M512 124.16v54.827l-302.293 294.187 676.949 0.043 38.827 38.784-38.827 38.784h-676.907l302.251 294.229v54.869h-56.32l-370.347-360.448v-54.869l370.347-360.405h56.32z" />
-                        </svg>
-                        </span>
-                        <div className="_2326- m-5 text-center">
-                            <h1> Dinamalar Classifieds  </h1>
-                        </div>
-                        <h3 className="_2SLog">
-                            <span>Enter your phone number</span>
-                        </h3>
-                        <div className="_2PVVq">
-                            <div className="rui-3OXDo">
-                                <label htmlFor="phone" />
-                                <div className="rui-2zp0U rui-2rouh">
-                                    <div className="rui-z4oOZ rui-3i1AN">
-                                        <div className="rui-3zt7h rui-iU02L rui-WrCgP">+91
-                                        </div>
-                                        <div className="rui-3APY9">
-                                            <input id="phone" name="phone" type="text" autoComplete="mobile" placeholder="Phone Number" className="rui-3vs1L rui-2LyaK undefined" defaultValue />
-                                            <div className="rui-1pgaV rui-Vcp5d" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="rui-1rV1O">
-                                    <span className="rui-3FLBC rui-_74YY" data-aut-id="error-phone" />
-                                </div>
-                            </div>
-                        </div>
-                        <button type="submit" data-aut-id className="rui-39-wj rui-3mpqt rui-1JPTg _2sWUW rui-38Vok" disabled><span>Next</span>
-                        </button>
-                        <p className="_3bCAe">
-                            <span>Your contact number is never shared with external parties nor do we use it to spam you in any way.</span>
-                        </p>
-                    </div>
-                </form>
-            </div>
-        </div>
-        </div>
-    )
+  },3000)
+}
+})
+
+
+  return (
+    <div>
+      <p id='successdis'></p>
+
+
+    <div style={{ width: "100%", display:"flex", justifyContent:"space-evenly", color: "black" }}>
+      <p id='dis'>Hello, {gname}</p>
+      <div style={{display:"block"}}>
+        <button
+          onClick={(e) => {
+            localStorage.removeItem("name");
+            // window.location.reload();
+            router.push('/')
+          }}
+          className="loginbtn"
+          id="logout"
+          style={{  backgroundColor: "red" }}>Logout</button>
+      </div>
+    </div>
+    </div>
+  )
 }
